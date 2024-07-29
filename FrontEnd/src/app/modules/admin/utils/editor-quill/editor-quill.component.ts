bb21e86db421ddaea42cd0d5262e92b40718b32a
dtamayo <franklin.tamayo@quito.gob.ec>
2024-07-29 11:36:22 -0500

    fx - opciones del editor de texto

--- FrontEnd/src/app/modules/admin/utils/editor-quill/editor-quill.component.ts
+++ FrontEnd/src/app/modules/admin/utils/editor-quill/editor-quill.component.ts
@@ -19,14 +19,14 @@ export class EditorQuillComponent {
   editorModules = {
     toolbar: [
       ['bold', 'italic', 'underline', 'strike'],
-      [{ 'header': 1 }, { 'header': 2 }],               // custom button values
-      [{ 'script': 'sub'}, { 'script': 'super' }], // superscript/subscript
+      /* [{ 'header': 1 }, { 'header': 2 }],  */              // custom button values
+      [{ 'script': 'super' }], // superscript/subscript
       [{ 'list': 'ordered' }, { 'list': 'bullet' }],     // superscript/subscript
       [{ 'indent': '-1' }, { 'indent': '+1' }],          // outdent/indent
-      [{ 'direction': 'rtl' }],                         // text direction
+      /* [{ 'direction': 'rtl' }], */                         // text direction
 
       [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
-      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+      /* [{ 'header': [1, 2, 3, 4, 5, 6, false] }], */
 
       [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
       [{ 'font': [] }],
